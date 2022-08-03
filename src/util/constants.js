97e0c91c5a8b210fb8c85a8c95889a2edf4df1d9
Harith Sankalpa <harith.sankalpa@syscolabs.com>
2022-08-03 23:47:40 +0530

    refactor: reduce default page size

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,5 +1,5 @@
 export const DOWNLOAD_DATE_PATTERN = 'DD_MMM_YYYY_hh_mm_ss';
-export const DEFAULT_PAGE_SIZE = 40;
+export const DEFAULT_PAGE_SIZE = 20;
 export const DATE_PATTERN = 'YYYY-MM-DD';
 export const DATE_PATTERN_DISPLAY = 'DD MMMM YYYY';
 export const COMMENT_TIME_PATTERN = 'DD MMMM YYYY hh:mm:ss';
