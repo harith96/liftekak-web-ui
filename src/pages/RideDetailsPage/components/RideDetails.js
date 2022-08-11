49abee1c81e74b8a78d4541b86b8aa0c62947419
Harith Sankalpa <harith.sankalpa@syscolabs.com>
2022-08-12 04:08:57 +0530

    feat: make ride details responsive

--- src/pages/RideDetailsPage/components/RideDetails.js
+++ src/pages/RideDetailsPage/components/RideDetails.js
@@ -3,23 +3,11 @@ import * as _ from 'lodash';
 
 import { getFormattedDate, getFormattedTime } from 'util/dateUtil';
 import getFullName from 'util/getFullName';
-import * as i18n from '_i18n';
 import { Col, Row, Spin } from 'antd';
 import getFormattedRoute from 'util/getFormattedRoute';
-import RideDetailsPageContext from '../RidesDetailsPageContext';
-import RideDetailsCard from './RideDetailsCard';
 import simulateCall from 'util/simulateCall';
-
-const renderRideDetail = (title, value) => {
-  return (
-    <div className="ride-field-container">
-      <p>
-        <span>{`${title}: `}</span>
-        <span>{value || ''}</span>
-      </p>
-    </div>
-  );
-};
+import RideDetailsPageContext from '../RidesDetailsPageContext';
+import RideDetailsColumn from '../RideDetailsColumn';
 
 function RideDetails() {
   const {
@@ -47,26 +35,14 @@ function RideDetails() {
           </Col>
         </Row>
         <Row gutter={[32, 32]}>
-          <Col span={6}>
-            <RideDetailsCard title="Start location" icon="right-circle" value={startLocation} />
-          </Col>
-          <Col span={6}>
-            <RideDetailsCard title="End location" icon="check-circle" value={destinationLocation} />
-          </Col>
-          <Col span={6}>
-            <RideDetailsCard title="Departure date" icon="calendar" value={getFormattedDate(departure)} />
-          </Col>
-          <Col span={6}>
-            <RideDetailsCard title="Departure time" icon="clock-circle" value={getFormattedTime(departure)} />
-          </Col>
+          <RideDetailsColumn title="Start location" icon="right-circle" value={startLocation} />
+          <RideDetailsColumn title="End location" icon="check-circle" value={destinationLocation} />
+          <RideDetailsColumn title="Departure date" icon="calendar" value={getFormattedDate(departure)} />
+          <RideDetailsColumn title="Departure time" icon="clock-circle" value={getFormattedTime(departure)} />
         </Row>
         <Row gutter={[32, 32]}>
-          <Col span={18}>
-            <RideDetailsCard title="Route" icon="fork" value={getFormattedRoute(route)} />
-          </Col>
-          <Col span={6}>
-            <RideDetailsCard title="Available seats" icon="number" value={availableSeatCount} />
-          </Col>
+          <RideDetailsColumn title="Route" icon="fork" value={getFormattedRoute(route)} lgColSpan={18} />
+          <RideDetailsColumn title="Available seats" icon="number" value={availableSeatCount} />
         </Row>
         <Row>
           <Col span={24}>
@@ -74,24 +50,19 @@ function RideDetails() {
           </Col>
         </Row>
         <Row gutter={[32, 32]}>
-          <Col span={8}>
-            <RideDetailsCard title="Driver name" icon="smile" value={getFullName(firstName, lastName)} />
-          </Col>
-          <Col span={8}>
-            <RideDetailsCard
-              title="Driver mobile no"
-              icon="phone"
-              value={mobileNo}
-              onClick={() => simulateCall(mobileNo)}
-            />
-          </Col>
-          <Col span={8}>
-            {_.isEmpty(driverNote) ? (
-              <RideDetailsCard title="Driver bio" icon="pic-left" value={bio} />
-            ) : (
-              <RideDetailsCard title="Driver note" icon="message" value={driverNote} />
-            )}
-          </Col>
+          <RideDetailsColumn title="Driver name" icon="smile" value={getFullName(firstName, lastName)} lgColSpan={8} />
+          <RideDetailsColumn
+            title="Driver mobile no"
+            icon="phone"
+            value={mobileNo}
+            onClick={() => simulateCall(mobileNo)}
+            lgColSpan={8}
+          />
+          {_.isEmpty(driverNote) ? (
+            <RideDetailsColumn title="Driver bio" icon="pic-left" value={bio} lgColSpan={8} />
+          ) : (
+            <RideDetailsColumn title="Driver note" icon="message" value={driverNote} lgColSpan={8} />
+          )}
         </Row>
         <Row>
           <Col span={24}>
@@ -99,18 +70,10 @@ function RideDetails() {
           </Col>
         </Row>
         <Row gutter={[32, 32]}>
-          <Col span={6}>
-            <RideDetailsCard title="Vehicle type" icon="car" value={vehicleType} />
-          </Col>
-          <Col span={6}>
-            <RideDetailsCard title="Vehicle brand" icon="car" value={brand} />
-          </Col>
-          <Col span={6}>
-            <RideDetailsCard title="Vehicle model" icon="car" value={model} />
-          </Col>
-          <Col span={6}>
-            <RideDetailsCard title="Vehicle color" icon="car" value={vehicleColor} />
-          </Col>
+          <RideDetailsColumn title="Vehicle type" icon="car" value={vehicleType} />
+          <RideDetailsColumn title="Vehicle brand" icon="car" value={brand} />
+          <RideDetailsColumn title="Vehicle model" icon="car" value={model} />
+          <RideDetailsColumn title="Vehicle color" icon="car" value={vehicleColor} />
         </Row>
       </div>
     </Spin>
